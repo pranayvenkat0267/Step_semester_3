20194a05b9f7eb86f5f161f1825f60fc015de201
pranayvenkat <pranayvenkatsamrla@gmail.com>
2026-09-05 23:20:22 +0530

    complete session 1 problems

--- src/main/java/JavaStringsConcept/assignment_problems/TrafficStreak.java
+++ src/main/java/JavaStringsConcept/assignment_problems/TrafficStreak.java
@@ -1,4 +1,42 @@
 package main.java.JavaStringsConcept.assignment_problems;
 
+import java.util.Scanner;
+
 public class TrafficStreak {
+
+    static void findLongestStreak(String signalLog) {
+
+        int currentCount = 1;
+        int maxCount = 1;
+        char longestChar = signalLog.charAt(0);
+
+        for (int i = 1; i < signalLog.length(); i++) {
+
+            if (signalLog.charAt(i) == signalLog.charAt(i - 1)) {
+                currentCount++;
+            } else {
+                currentCount = 1;
+            }
+
+            if (currentCount > maxCount) {
+                maxCount = currentCount;
+                longestChar = signalLog.charAt(i);
+            }
+        }
+
+        System.out.println("Longest Streak: '" + longestChar
+                + "' repeated " + maxCount + " times");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter signal log: ");
+        String signalLog = sc.nextLine();
+
+        findLongestStreak(signalLog);
+
+        sc.close();
+    }
 }
--- src/main/java/JavaStringsConcept/class_problems/RockPaperScissors.java
+++ src/main/java/JavaStringsConcept/class_problems/RockPaperScissors.java
@@ -1,4 +1,78 @@
 package main.java.JavaStringsConcept.class_problems;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class RockPaperScissors {
+
+    static String playRound(String playerMove, String computerMove) {
+
+        if (playerMove.equals(computerMove)) {
+            return "Draw";
+        }
+
+        if ((playerMove.equals("Rock") && computerMove.equals("Scissors")) ||
+                (playerMove.equals("Paper") && computerMove.equals("Rock")) ||
+                (playerMove.equals("Scissors") && computerMove.equals("Paper"))) {
+            return "Player Wins";
+        }
+
+        return "Computer Wins";
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] moves = {"Rock", "Paper", "Scissors"};
+
+        String[] playerMoves = new String[5];
+        String[] computerMoves = new String[5];
+        String[] results = new String[5];
+
+        int wins = 0;
+        int losses = 0;
+        int draws = 0;
+
+        for (int i = 0; i < 5; i++) {
+
+            System.out.print("Enter Player Move (Rock/Paper/Scissors): ");
+            playerMoves[i] = sc.next();
+
+            computerMoves[i] = moves[random.nextInt(3)];
+
+            results[i] = playRound(playerMoves[i], computerMoves[i]);
+
+            if (results[i].equals("Player Wins")) {
+                wins++;
+            } else if (results[i].equals("Computer Wins")) {
+                losses++;
+            } else {
+                draws++;
+            }
+
+            System.out.println("Computer Move: " + computerMoves[i]);
+            System.out.println("Result: " + results[i]);
+            System.out.println();
+        }
+
+        System.out.println("Round\tPlayer Move\tComputer Move\tResult");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println((i + 1) + "\t" +
+                    playerMoves[i] + "\t\t" +
+                    computerMoves[i] + "\t\t" +
+                    results[i]);
+        }
+
+        double winPercentage = (wins / 5.0) * 100;
+
+        System.out.println("\nWins: " + wins);
+        System.out.println("Losses: " + losses);
+        System.out.println("Draws: " + draws);
+        System.out.println("Win % = " + winPercentage);
+
+        sc.close();
+    }
 }
